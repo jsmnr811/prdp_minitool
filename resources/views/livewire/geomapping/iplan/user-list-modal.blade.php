8c0aa378483925562f2e4d6b1587e4fc7e5ab892
Jasmin Aure <jasmin.aure.4@gmail.com>
2025-09-09 21:31:11 +0800

    s

--- resources/views/livewire/geomapping/iplan/user-list-modal.blade.php
+++ resources/views/livewire/geomapping/iplan/user-list-modal.blade.php
@@ -243,7 +243,6 @@ new class extends Component {
         }
         // Generate a PNG snapshot of the HTML
         Browsershot::html($html)
-            ->setChromeExecutablePath('/usr/bin/chromium-browser')
             ->windowSize(330, 520) // match your ID card width & height
             ->waitUntilNetworkIdle() // ensures images/fonts are loaded
             ->save($storagePath);
