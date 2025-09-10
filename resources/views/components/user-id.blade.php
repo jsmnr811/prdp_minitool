733f8051e11ca39936375d05039c59b010011d27
jsmnr811 <jasmin.aure.4@gmail.com>
2025-09-10 11:14:47 +0800

    .

--- resources/views/components/user-id.blade.php
+++ resources/views/components/user-id.blade.php
@@ -16,66 +16,70 @@
         {{-- User Info --}}
         <div style="display:flex; flex-direction:column; align-items:center; margin-top:5px;">
             @php
-                $firstNameWords = explode(' ', strtoupper($user->firstname));
-                $firstName =
-                    count($firstNameWords) > 1 ? $firstNameWords[0] . ' ' . $firstNameWords[1] : $firstNameWords[0];
-                $lastName = strtoupper($user->lastname);
+            $firstNameWords = explode(' ', strtoupper($user->firstname));
+            $firstName =
+            count($firstNameWords) > 1 ? $firstNameWords[0] . ' ' . $firstNameWords[1] : $firstNameWords[0];
+            $lastName = strtoupper($user->lastname);
 
-                $firstFontSize = 32;
-                $lastFontSize = 18;
-                $maxWidth = 250 - 8;
+            $firstFontSize = 32;
+            $lastFontSize = 18;
+            $maxWidth = 250 - 8;
 
-                if (strlen($firstName) > 8) {
-                    $approxCharWidth = 12;
-                    $nameWidth = strlen($firstName) * $approxCharWidth;
-                    while ($nameWidth > $maxWidth && $firstFontSize > 10) {
-                        $firstFontSize--;
-                        $approxCharWidth = 0.5 * $firstFontSize;
-                        $nameWidth = strlen($firstName) * $approxCharWidth;
-                    }
-                }
+            if (strlen($firstName) > 8) {
+            $approxCharWidth = 12;
+            $nameWidth = strlen($firstName) * $approxCharWidth;
+            while ($nameWidth > $maxWidth && $firstFontSize > 10) {
+            $firstFontSize--;
+            $approxCharWidth = 0.5 * $firstFontSize;
+            $nameWidth = strlen($firstName) * $approxCharWidth;
+            }
+            }
 
-                $approxCharWidth = 9;
-                $nameWidth = strlen($lastName) * $approxCharWidth;
-                while ($nameWidth > $maxWidth && $lastFontSize > 8) {
-                    $lastFontSize--;
-                    $approxCharWidth = 0.5 * $lastFontSize;
-                    $nameWidth = strlen($lastName) * $approxCharWidth;
-                }
+            $approxCharWidth = 9;
+            $nameWidth = strlen($lastName) * $approxCharWidth;
+            while ($nameWidth > $maxWidth && $lastFontSize > 8) {
+            $lastFontSize--;
+            $approxCharWidth = 0.5 * $lastFontSize;
+            $nameWidth = strlen($lastName) * $approxCharWidth;
+            }
             @endphp
 
             <div class="name"
                 style="text-align:center; margin-top:0px; white-space:nowrap; overflow:hidden; padding-left:4px; padding-right:4px;">
                 <div
-                    style="font-family:'NeulisBlack', Arial, sans-serif; font-size: {{ $firstFontSize }}px; font-weight:900; color:#2f4482; line-height:1.1;">
+                    style="font-family:'Arial Black', Arial, sans-serif; font-size: {{ $firstFontSize }}px; font-weight:900; color:#2f4482; line-height:1.1;">
                     {{ $firstName }}
                 </div>
                 <div
-                    style="font-family:'NeulisMediumItalic', Arial, sans-serif; font-size: {{ $lastFontSize }}px; font-style:italic; color:#2f4482; line-height:1.1;">
+                    style="font-family:Arial, sans-serif; font-size: {{ $lastFontSize }}px; font-style:italic; color:#2f4482; line-height:1.1; font-weight:600;">
                     {{ $lastName }}
                 </div>
             </div>
             <div
-                style="text-align:center; font-family:'NeulisSansBold', Arial, sans-serif; font-size:1rem; color:#d3e6e9; margin-top:5px;">
+                style="text-align:center; font-family: Arial, sans-serif; font-size:1rem; color:#d3e6e9; margin-top:5px; font-weight:800;">
                 {{ strtoupper($user->designation) }}
             </div>
         </div>
+        <div style="position: relative; margin-top: 5px; height: 130px; display: flex; align-items: center; justify-content: flex-start;">
 
-        <div style="position: relative; margin-top: 15px; height: 130px;">
-            {{-- QR Code --}}
-            <div
-                style="position: absolute; left: 30%; top: 0; transform: translateX(-50%);
-        width: 120px; height: 120px; display:flex; justify-content:center; align-items:center;">
+            {{-- QR Code centered absolutely --}}
+            <div style="position: absolute; left: 50%; transform: translateX(-50%); width: 120px; height: 120px; display:flex; justify-content:center; align-items:center;">
                 {!! SimpleSoftwareIO\QrCode\Facades\QrCode::size(110)->margin(2)->generate(route('investment.user-verification', ['id' => $user->id])) !!}
             </div>
 
-            {{-- Text on the right, bottom-aligned with QR --}}
-            <div
-                style="position: absolute; right: 0%; top: 0; bottom:-50px;
-               bottom: 0; font-family:'NeulisSansBold', Arial, sans-serif; font-size:1rem; color:#2f4482;">
-                ExampleWord
-            </div>
+            {{-- Text beside QR --}}
+            <div style="margin-top: 75px; margin-left: 200px; font-family:'NeulisSansBold', Arial, sans-serif; font-size:.8rem; color:#2f4482; line-height:1.2; text-align: left;">
+    <div style="font-family: Arial, sans-serif; font-size:.8rem; font-weight: 700;">ID No:</div>
+    <div style="font-family: Arial, sans-serif; font-size:.8rem; font-weight: 800;">{{ $user->login_code }}</div>
+</div>
+
+
+
+
         </div>
+
+
+
     </div>
 
 
