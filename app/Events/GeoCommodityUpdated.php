332db5dc3291853d4ab727b644a333243091e567
jsmnr811 <jasmin.aure.4@gmail.com>
2025-09-15 19:37:52 +0800

    --

--- app/Events/GeoCommodityUpdated.php
+++ app/Events/GeoCommodityUpdated.php
@@ -1,13 +1,16 @@
 <?php
 
+namespace App\Events;
+
 use Illuminate\Broadcasting\Channel;
-use Illuminate\Contracts\Broadcasting\ShouldBroadcastNow;
-use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
+use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcastNow;
 
 class GeoCommodityUpdated implements ShouldBroadcastNow
 {
+
     use Dispatchable, InteractsWithSockets, SerializesModels;
 
     public $message;
